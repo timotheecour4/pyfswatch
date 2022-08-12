2f2f3b4ee59ba40e104b0c3039357d3ffb93ed6f
Timothee Cour <ticour@amazon.com>
2022-08-12 00:23:27 -0700

    setup.py: pyfswatch=>fswatch

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyfswatch",
+    # pending https://github.com/paul-nameless/pyfswatch/issues/9
+    name="fswatch",
     version="0.1",
     description="Python bindings for libfswatch.",
     packages=find_packages(),
